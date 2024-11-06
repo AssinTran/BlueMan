3f35c73aac593cf7d4d59301b315b7131e415a5d
Cong Chinh(Assin) <126130000+AscAssin@users.noreply.github.com>
2024-11-06 17:22:48 +0700

    Add interface repo

---
+++ Repository/IRepo/IProductRepo.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.IRepo
+{
+    public interface IProductRepo
+    {
+        Task<List<Product>> ListProducts();
+        Task<Product?> GetProduct(string productId);
+        Task<bool> AddNew(Product product);
+        Task<bool> Delete(string productId);
+        Task<bool> Update(Product product);
+    }
+}
---
+++ Repository/IRepo/IReviewRepo.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.IRepo
+{
+    public interface IReviewRepo
+    {
+        Task<List<Review>> ListReview();
+        Task<List<Review>> ListReviewProduct(string productId);
+        Task<Review?> GetReview(string productId);
+        Task<bool> AddReview(Review review);
+        Task<bool> Delete(string reviewId);
+    }
+}
